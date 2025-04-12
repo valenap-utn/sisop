20d5202bb9a8368df5028fe223f3990852aaf7f9
Santiago Ozan <santiagoozan97@gmail.com>
2025-04-12 18:18:26 -0300

    cambio nombres de launch

--- io/src/io.c
+++ io/src/io.c
@@ -1,5 +1,9 @@
 #include <utils/utils.h>
 
 int main(int argc, char* argv[]) {
+    //printf(argv[argc-1]); // ejemplo de ultimo argumento, o nombre del dispositivo
+    if(argc == 1){
+        return 0;
+    }
     return 0;
 }
