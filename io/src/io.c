03369a1005f0fd12c57c67f812725af111c91613
Santiago Ozan <santiagoozan97@gmail.com>
2025-04-22 20:17:21 -0300

    conxion io_kernel parte 2

--- io/src/io.c
+++ io/src/io.c
@@ -4,9 +4,16 @@
 t_log *logger;
 t_config *config;
 t_log_level current_log_level;
+
+char *nombre_modulo_io;
 //variables globales
 
 int main(int argc, char* argv[]) {
+    
+    nombre_modulo_io = argv[argc-1];
+    
     inicializarIo();
+
+    log_debug(logger, nombre_modulo_io);
     return 0;
 }
