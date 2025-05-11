86d7c6c60e6078914ed9a235a2c1f6f1f70865bc
utnso <vmanini@frba.utn.edu.ar>
2025-05-11 21:06:05 +0000

    Se cambio de lugar las intrucciones

--- cpu/src/cpu_utilities.h
+++ cpu/src/cpu_utilities.h
@@ -8,23 +8,5 @@ void levantarConfig();
 void *conexion_cliente_kernel(void *args);
 void *conexion_cliente_memoria(void *args);
 
-void Feth();
-void Decode();
-void Execute();
-void Execute();
-void Check_Int();
-
-typedef enum {
-    // Syscalls
-    IO,
-    INIT_PROC,
-    DUMP_MEMORY,
-    EXIT,
-    // No Syscalls
-    NOOP,
-    WRITE,
-    READ,
-    GOTO
-}intrucciones_t ;
 
 #endif
---
+++ cpu/src/instrucciones.c
@@ -0,0 +1,28 @@
+#include <instrucciones.h>
+
+void ciclo_instruccion(){
+	while ((1)){
+        Feth();
+        Decode();
+        Execute();
+        Execute();
+        Check_Int();
+	}
+}
+
+
+void Feth(){
+
+};
+void Decode(){
+    
+};
+void Execute(){
+
+};
+void Execute(){
+
+};
+void Check_Int(){
+
+};
