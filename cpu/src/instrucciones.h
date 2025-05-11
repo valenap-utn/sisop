3072906bec7d442cb3ed5ea65d6a6c2e0c5e9b66
utnso <vmanini@frba.utn.edu.ar>
2025-05-11 21:06:05 +0000

    Se cambio de lugar las intrucciones

--- cpu/src/instrucciones.h
+++ cpu/src/instrucciones.h
@@ -10,5 +10,23 @@ void write(uint32_t* direccion, uint32_t* datos);
 void read(uint32_t* direccion, int tamanio);
 void goto_(int valor);
 
+void Feth();
+void Decode();
+void Execute();
+void Execute();
+void Check_Int();
+
+typedef enum {
+    // Syscalls
+    IO,
+    INIT_PROC,
+    DUMP_MEMORY,
+    EXIT,
+    // No Syscalls
+    NOOP,
+    WRITE,
+    READ,
+    GOTO
+}intrucciones_t ;
 
 #endif
