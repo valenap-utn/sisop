6324c3c2baac8565db7ca220caee53034e5114a3
Santi Ozan <santiagoozan97@gmail.com>
2025-07-11 21:28:28 -0300

    part 2

--- cpu/src/cpu_utilities.h
+++ cpu/src/cpu_utilities.h
@@ -8,5 +8,6 @@ void levantarConfig();
 void *conexion_cliente_kernel(void *args);
 void *conexion_cliente_memoria(void *args);
 
+void encolar_interrupcion_generico(list_struct_t *cola, interrupcion_t *interrupcion);
 
 #endif
